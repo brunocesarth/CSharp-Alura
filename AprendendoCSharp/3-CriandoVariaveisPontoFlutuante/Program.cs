3bb4a93c947b7b1231df2893c37fd7a1b9700e3b
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-02-16 11:09:40 -0300

    CSharp e OO

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int n = 1;
+        int fatorial = 1;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine(fatorial);
+        }
+    }
+}
