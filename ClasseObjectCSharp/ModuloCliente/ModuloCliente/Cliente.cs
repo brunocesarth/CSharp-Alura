d61a6e6dcef1b130bb97ed33ddf7a2ae1ec427a7
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-02-16 20:48:43 -0300

    Classe Object C#

---
+++ ClasseObjectCSharp/ModuloCliente/ModuloCliente/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCliente
+{
+    class Cliente
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, \n" +
+                $"Email: {Email}, \n" +
+                $"Idade: {Idade},\n" +
+                $"CPF: {Cpf}";
+        }
+    }
+}
---
+++ ClasseObjectCSharp/ModuloCliente/ModuloCliente/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ModuloCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "André";
+            cliente.Email = "andre@email.com";
+            cliente.Cpf = "111.444.666.88";
+            cliente.Idade = 23;
+
+            Console.WriteLine(cliente);
+        }
+    }
+}
