01dd688e261b6e94ba91f080b794208366da634b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-02-17 21:11:33 -0300

    Desafio 2

---
+++ CSharpHerancaInterface/Desafio2/Desafio2/Desafio2/Entities/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2.Entities
+{
+    class Livro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public DateTime AnoPublicacao { get; set; }
+        public string Categoria { get; set; }
+        public int NumeroDePaginas { get; set; }
+        public string PaisDePublicacao { get; set; }
+
+        public Livro(string titulo, string iSBN)
+        {
+            Titulo = titulo;
+            ISBN = iSBN;
+        }
+
+        public override string ToString()
+        {
+            return $"Livro: {Titulo} / ISBN: {ISBN}";
+        }
+    }
+}
---
+++ CSharpHerancaInterface/Desafio2/Desafio2/Desafio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Desafio2.Entities;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro("O Nome do Vento", "8599296493");
+            Livro livro2 = new Livro("O Teste do Fogo", "4654653462");
+            Livro livro3 = new Livro("A Cura da Água", "4654653813");
+            Livro livro4 = new Livro("A Maldição da Terra", "4654653462");
+
+            Console.WriteLine(livro1);
+            Console.WriteLine(livro2);
+            Console.WriteLine(livro3);
+            Console.WriteLine(livro4);
+        }
+    }
+}
