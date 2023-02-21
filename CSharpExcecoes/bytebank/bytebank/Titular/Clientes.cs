4baa76d5b4dc04a0e16db2972deda28ad5140825
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-02-20 21:28:22 -0300

    Aula 1 - Exceções

---
+++ CSharpExcecoes/bytebank/bytebank/Titular/Clientes.cs
@@ -0,0 +1,11 @@
+﻿namespace bytebank.Titular
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissap { get; set; }
+
+        public Cliente() { }
+    }
+}
