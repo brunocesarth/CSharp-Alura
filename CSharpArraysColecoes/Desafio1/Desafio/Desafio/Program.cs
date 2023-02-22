d442e792c48d25ec6a3d4e6ecc73422ea133eeac
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-02-22 10:11:49 -0300

    Desafio Parte 1

---
+++ CSharpArraysColecoes/Desafio1/Desafio/Desafio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] array = new double[5];
+            array.SetValue(17.5, 0);
+            array.SetValue(19.3, 0);
+            array.SetValue(43.3, 0);
+            array.SetValue(63.2, 0);
+            array.SetValue(8.5, 0);
+
+            DescobreMedia(array);
+            double valorTotal = 0;
+
+            void DescobreMedia(double[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    valorTotal += array[i];
+                }
+                double media = valorTotal / array.Length;
+                Console.WriteLine(media);
+            }
+        }
+    }
+}
--- CSharpArraysColecoes/bytebank_ATENDIMENTO/bytebank_ATENDIMENTO/Program.cs
+++ CSharpArraysColecoes/bytebank_ATENDIMENTO/bytebank_ATENDIMENTO/Program.cs
@@ -6,7 +6,8 @@ namespace bytebank_ATENDIMENTO
     {
         static void Main(string[] args)
         {
-            TestaArrayInt();
+            //TestaArrayInt();
+            //TestaBuscarPalavra();
 
             void TestaArrayInt()
             {
@@ -28,6 +29,60 @@ namespace bytebank_ATENDIMENTO
 
                 int media = acumulador / idades.Length;
                 Console.WriteLine($"Média de idades = {media}");
+            }
+
+            void TestaBuscarPalavra()
+            {
+                string[] arrayDePalavras = new string[5];
+                for (int i = 0; i < arrayDePalavras.Length; i++)
+                {
+                    Console.Write($"Digite {i + 1}ª palavra: ");
+                    arrayDePalavras[i] = Console.ReadLine();
+                }
+
+                Console.WriteLine("Digite uma palavra a ser encontrada: ");
+                ;
+                var busca = Console.ReadLine();
+
+                foreach (string palavra in arrayDePalavras)
+                {
+                    if (palavra.Equals(busca))
+                    {
+                        Console.WriteLine($"Palavra encontrada = {busca}.");
+                        break;
+                    }
+                }
+            }
+
+            Array amostra = Array.CreateInstance(typeof(double), 5);
+            amostra.SetValue(5.9, 0);
+            amostra.SetValue(1.8, 1);
+            amostra.SetValue(7.1, 2);
+            amostra.SetValue(10, 3);
+            amostra.SetValue(6.9, 4);
+
+            //TestaMediana(amostra);
+            void TestaMediana(Array array)
+            {
+                if ((array == null) || (array.Length == 0))
+                {
+                    Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+                }
+
+                double[] numerosOrdenados = (double[])array.Clone();
+                Array.Sort(numerosOrdenados);
+
+                int tamanho = numerosOrdenados.Length;
+                int meio = tamanho / 2;
+                double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+                Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+            }
+
+            int[] valores = { 10, 58, 36, 47 };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(valores[i]);
             }
         }
     }
