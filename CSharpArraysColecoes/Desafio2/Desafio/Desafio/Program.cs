c2395a35ba52a0524005a3f53fbe7e94933dc44a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-02-25 17:51:30 -0300

    Desafio

---
+++ CSharpArraysColecoes/Desafio2/Desafio/Desafio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+BuscarNome();
+void BuscarNome()
+{
+    Console.Write("Digite um nome para procurar: ");
+    string nomeBusca = Console.ReadLine();
+
+    foreach(string nome in nomesDosEscolhidos)
+    {
+        if (nome.Equals(nomeBusca))
+        {
+            Console.WriteLine($"Nome encontrado = {nomeBusca}.");
+            break;
+        }
+    }
+}
+
