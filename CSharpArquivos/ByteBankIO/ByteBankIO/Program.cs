d69c9ec55cd5ab7feac96722c5e36e37c5b4c165
Bruno Thobias <bruno.cm.thobias@hotmail.com>
2023-03-16 00:38:32 -0300

    Update Program.cs

--- CSharpArquivos/ByteBankIO/ByteBankIO/Program.cs
+++ CSharpArquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.IO;
 
 partial class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+
+        using(var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+            // var linha = leitor.ReadLine();
+
+            // var texto = leitor.ReadToEnd();
+
+            //var numero = leitor.Read(); //Trás o primeiro byte do arquivo
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
         Console.ReadLine();
     }
 }
