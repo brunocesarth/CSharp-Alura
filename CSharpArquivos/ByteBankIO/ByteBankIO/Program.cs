968a945f4eff6da171d921f4d01ffab2d8739308
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-03-17 19:27:37 -0300

    Finalização do Curso

--- CSharpArquivos/ByteBankIO/ByteBankIO/Program.cs
+++ CSharpArquivos/ByteBankIO/ByteBankIO/Program.cs
@@ -6,13 +6,21 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        EscritaBinaria();
-        LeituraBinaria();
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        //foreach(var linha in linhas)
+        //{
+        //    Console.WriteLine(linha);
+        //}
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
 
         Console.WriteLine("Aplicação Finalizada ...");
 
         Console.ReadLine();
     }
-
-
 }
