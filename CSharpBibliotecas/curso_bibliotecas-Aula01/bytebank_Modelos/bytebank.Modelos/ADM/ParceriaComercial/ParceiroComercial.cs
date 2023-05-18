bc8b07200c20a5a3b6a0ece2245d13f22e1b243c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-05-18 20:20:59 -0300

    Aula 2

--- CSharpBibliotecas/curso_bibliotecas-Aula01/bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
+++ CSharpBibliotecas/curso_bibliotecas-Aula01/bytebank_Modelos/bytebank.Modelos/ADM/ParceriaComercial/ParceiroComercial.cs
@@ -1,14 +1,17 @@
 ﻿using bytebank.Modelos.ADM.SistemaInterno;
+using bytebank_Modelos.bytebank.Modelos.ADM.Utilitario;
 
 namespace bytebank.Modelos.ADM.Utilitario
 {
     public class ParceiroComercial : IAutenticavel
     {
+        private AutenticacaoUtil Autenticador = new AutenticacaoUtil();
+
         public string Senha { get; set; }
-      
+
         public bool Autenticar(string senha)
         {
-            return this.Senha == senha;
+            return this.Autenticador.ValidarSenha(this.Senha, senha);
         }
     }
 }
