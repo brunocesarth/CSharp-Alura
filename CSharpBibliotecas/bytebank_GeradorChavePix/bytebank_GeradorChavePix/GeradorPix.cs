418ab851f227aa0fdd161a974de1a84cb5884e18
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2023-05-19 20:29:59 -0300

    Finalização do curso

--- CSharpBibliotecas/bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
+++ CSharpBibliotecas/bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
@@ -1,17 +1,24 @@
 ﻿namespace bytebank_GeradorChavePix
 {
+    /// <summary>
+    ///  Classe que gera chaves Pix usando o formato Guid.
+    /// </summary>
     public static class GeradorPix
     {
+        /// <summary>
+        /// Método que retorna uma chave aleatória de Pix
+        /// </summary>
+        /// <returns>Retorna uma chave PIX no formato String</returns>
         public static string GetChavePix()
         {
             return Guid.NewGuid().ToString();
         }
 
-        public static List<object> GetList()
-        {
-            return new List<object>();
-        }
-
+        /// <summary>
+        /// Método que retorna uma lista aleatória de chaves PIX.
+        /// </summary>
+        /// <param name="numeroDeChaves"> Quantidade de chaves a serem geradas.</param>
+        /// <returns>Lista de strings de chaves Pix.</returns>
         public static List<string> GetChavesPix(int numeroDeChaves)
         {
             if (numeroDeChaves <= 0)
